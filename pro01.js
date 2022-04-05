eade41b360864dfaca0cd3bf85c45dc4e75c8c6a
shayglik <glik99@gmail.com>
2022-04-05 19:37:24 +0300

    shuffle

--- pro01.js
+++ pro01.js
@@ -9,11 +9,27 @@ const cards = ["A", "B", "C", "A","B","C"];
 
 // board.appendChild(element);
 
+
+function creatCard(idx){
+    const element = document.createElement("div");
+        element.innerHTML=cards[idx]
+        element.id = idx
+        element.className = "card"
+        return element
+}
+
+function shuffleArray(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+
+
 function newCards(cards){
-    for (i of cards){
-        const element = document.createElement("div");
-        element.innerHTML=i;
-        const board = document.getElementById("board");
+    shuffleArray(cards)
+    const board = document.getElementById("board");
+    for (i in cards){
+        // const element = document.createElement("div");
+        // element.innerHTML=cards[i];
+        let element = creatCard(i)
         board.appendChild(element);    
     }
 }
